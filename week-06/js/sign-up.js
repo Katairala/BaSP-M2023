59c307360996ad697468bdad033f7848290657d9
Katerine Airala <katherine.airala@gmail.com>
2023-04-25 19:20:18 -0300

    BaSP-W06-2023: finished sign up

--- week-06/js/sign-up.js
+++ week-06/js/sign-up.js
@@ -1,3 +1,4 @@
+var submitButton = document.getElementById('submit');
 var nameInput = document.getElementById('name');
 var lastNameInput = document.getElementById('lastname');
 var emailInput = document.getElementById('email');
@@ -5,6 +6,10 @@ var passwordField = document.getElementById('password');
 var repeatPasswordField = document.getElementById('Repeat');
 var dateField = document.getElementById('date');
 var IDField = document.getElementById('ID');
+var phoneField = document.getElementById('phone');
+var zipInput = document.getElementById('zip');
+var cityInput = document.getElementById('City');
+var addressInput = document.getElementById('address');
 
 nameInput.addEventListener('blur', validateName);
 nameInput.addEventListener('focus', clearNameError);
@@ -15,10 +20,19 @@ emailInput.addEventListener('focus', clearEmail);
 passwordField.addEventListener('blur', validatePassword);
 passwordField.addEventListener('focus', clearPassword);
 repeatPasswordField.addEventListener('blur', validateRepeatPassword);
+repeatPasswordField.addEventListener('focus', clearRepeatPassword);
 dateField.addEventListener('blur', validateDate);
 dateField.addEventListener('focus', clearDate);
 IDField.addEventListener('blur', validateID);
 IDField.addEventListener('focus', clearID);
+phoneField.addEventListener('blur', validatePhone);
+phoneField.addEventListener('focus', clearPhone);
+zipInput.addEventListener('blur', validateZip);
+zipInput.addEventListener('focus', clearZip);
+cityInput.addEventListener('blur', validateCity);
+cityInput.addEventListener('focus', clearCity);
+addressInput.addEventListener('blur', validateAddress);
+addressInput.addEventListener('focus', clearAddress);
 
 function validateName() {
   var name = nameInput.value.trim();
@@ -126,9 +140,8 @@ function validateRepeatPassword() {
   return true;
 }
 
-function clearPassword() {
-  passwordField.value = '';
-  document.getElementById('password-error').textContent = '';
+function clearRepeatPassword() {
+  document.getElementById('repeat-error').textContent = '';
 }
 
 function validateDate() {
@@ -136,7 +149,7 @@ function validateDate() {
   var dateOfBirth = new Date(dateInput);
   var currentDate = new Date();
   var adultDate = new Date();
-  adultDate.setFullYear(currentDate.getFullYear() - 18); 
+  adultDate.setFullYear(currentDate.getFullYear() - 18);
   if (dateOfBirth > adultDate) {
     document.getElementById('date-error').textContent = 'You must be an adult';
   }
@@ -151,13 +164,15 @@ function validateID() {
   for (var i = 0; i < idInput.length; i++) {
     var charCode = idInput.charCodeAt(i);
     if (charCode < 48 || charCode > 57) {
-      document.getElementById('id-error').textContent = 'ID must contain only numbers';
+      document.getElementById('id-error').textContent =
+        'ID must contain only numbers';
       return;
     }
   }
 
   if (idInput.length < 8) {
-    document.getElementById('id-error').textContent = 'ID must be at least 8 digits long';
+    document.getElementById('id-error').textContent =
+      'ID must be at least 8 digits long';
     return;
   }
 }
@@ -166,8 +181,155 @@ function clearID() {
   document.getElementById('id-error').textContent = '';
 }
 
+function validatePhone() {
+  var phone = this.value;
+  var phoneError = document.getElementById('phone-error');
+  var numbers = '0123456789';
+  for (var i = 0; i < phone.length; i++) {
+    if (numbers.indexOf(phone[i]) === -1) {
+      phoneError.textContent = 'Phone number field must contain only numbers';
+      return;
+    }
+  }
+  if (phone.length !== 10) {
+    phoneError.textContent = 'Phone number field must have 10 digits';
+  } else {
+    phoneError.textContent = '';
+  }
+}
 
+function clearPhone() {
+  var phoneError = document.getElementById('phone-error');
+  phoneError.textContent = '';
+}
 
+function validateZip() {
+  var zip = this.value;
+  var zipError = document.getElementById('zip-error');
+  var numbers = '0123456789';
+  for (var i = 0; i < zip.length; i++) {
+    if (numbers.indexOf(zip[i]) === -1) {
+      zipError.textContent =
+        'Zip must contain only numbers and have between 4 and 5 digits';
+      return;
+    }
+  }
+  if (zip.length < 4 || zip.length > 5) {
+    zipError.textContent =
+      'Zip must contain only numbers and have between 4 and 5 digits';
+  } else {
+    zipError.textContent = '';
+  }
+}
 
+function clearZip() {
+  var zipError = document.getElementById('zip-error');
+  zipError.textContent = '';
+}
 
+function validateCity() {
+  var city = this.value;
+  var cityError = document.getElementById('city-error');
+  var alphanumeric =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  if (city.length < 3) {
+    cityError.textContent =
+      'City must contain at least 3 alphanumeric characters';
+  } else {
+    for (var i = 0; i < city.length; i++) {
+      if (alphanumeric.indexOf(city[i]) === -1) {
+        cityError.textContent =
+          'City must contain at least 3 alphanumeric characters';
+        return;
+      }
+    }
+    cityError.textContent = '';
+  }
+}
 
+function clearCity() {
+  var cityError = document.getElementById('city-error');
+  cityError.textContent = '';
+}
+
+function validateAddress() {
+  var address = this.value;
+  var addressError = document.getElementById('address-error');
+  var alphanumeric =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+  if (address.length < 5) {
+    addressError.textContent =
+      'Address must contain at least 5 characters with letters and numbers';
+  } else {
+    var spaceIndex = address.indexOf(' ');
+    if (
+      spaceIndex === -1 ||
+      spaceIndex === 0 ||
+      spaceIndex === address.length - 1
+    ) {
+      addressError.textContent =
+        'Address must contain at least 5 characters with letters and numbers';
+    } else {
+      var firstPart = address.substring(0, spaceIndex);
+      var secondPart = address.substring(spaceIndex + 1);
+      for (var i = 0; i < firstPart.length; i++) {
+        if (alphanumeric.indexOf(firstPart[i]) === -1) {
+          addressError.textContent =
+            'Address must contain at least 5 characters with letters and numbers';
+          return;
+        }
+      }
+      addressError.textContent = '';
+    }
+  }
+}
+
+function clearAddress() {
+  var addressError = document.getElementById('address-error');
+  addressError.textContent = '';
+}
+
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  var signUp =
+    validateName() &&
+    validateLastName() &&
+    validateEmail() &&
+    validatePassword() &&
+    validateRepeatPassword() &&
+    validateDate() &&
+    validateID() &&
+    validatePhone() &&
+    validateZip() &&
+    validateCity() &&
+    validateAddress()
+  if (signUp) {
+    alert(
+      'Name: ' +
+        name +
+        '\nLast name: ' +
+        lastname +
+        '\nID: ' +
+        id +
+        '\nDate of birth: ' +
+        date +
+        '\nPhone number: ' +
+        phone +
+        '\nAddress: ' +
+        address +
+        '\nCity: ' +
+        city +
+        '\nZip code: ' +
+        zip +
+        '\nEmail: ' +
+        email +
+        '\nPassword: ' +
+        password +
+        '\nRepeat password: ' +
+        repeat
+    );
+  } else {
+    alert('One or more of the fields are not valid.');
+  }
+});
